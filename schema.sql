29737dea0b22b09df4a177e9f7095155894dceb3
Nayanyadav2750 <nayanyadav208@gmail.com>
2026-08-12 17:04:35 +0530

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE e_sales (
+    order_id VARCHAR(20) NOT NULL,
+    customer_id VARCHAR(20),
+    product_id VARCHAR(20),
+    category VARCHAR(50),
+    price DECIMAL(10,2),
+    discount DECIMAL(5,2),
+    quantity INT,
+    payment_method VARCHAR(50),
+    order_date DATE,
+    delivery_time_days INT,
+    region VARCHAR(50),
+    returned VARCHAR(10),
+    total_amount DECIMAL(10,2),
+    shipping_cost DECIMAL(10,2),
+    profit_margin DECIMAL(5,2),
+    customer_age INT,
+    customer_gender VARCHAR(20),
+    PRIMARY KEY (order_id)
+);
